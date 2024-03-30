ccfac8fe406a97ef96905e19a45c8fb93f47d2ac
yegor52 <yegorbeat2005@gmail.com>
2024-03-30 19:48:56 +0300

    ADS-5 Sharov

--- include/tstack.h
+++ include/tstack.h
@@ -7,11 +7,11 @@
 
 template<typename T, int size>
 class TStack {
-private :
+ private :
     T* data;
     int top;
 
-public :
+ public :
     TStack() : top(-1) {
         data = new T[size];
     }
