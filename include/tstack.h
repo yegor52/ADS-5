f2c5eff2a49f3fa1b70c8c36b5f4ed8296759d31
yegor52 <yegorbeat2005@gmail.com>
2024-03-30 19:39:56 +0300

    ADS-5 Sharov

--- include/tstack.h
+++ include/tstack.h
@@ -7,47 +7,43 @@
 
 template<typename T, int size>
 class TStack {
-private:
-    T* data;
-    int top;
+    private:
+        T* data;
+        int top;
 
-public:
-    TStack() : top(-1) {
-        data = new T[size];
-    }
-
-    ~TStack() {
-        delete[] data;
-    }
-
-    void push(T value) {
-        if (top == size - 1) {
-            throw std::overflow_error("Stack overflow");
+    public:
+        TStack() : top(-1) {
+            data = new T[size];
         }
-        data[++top] = value;
-    }
 
-    T pop() {
-        if (top == -1) {
-            throw std::underflow_error("Stack underflow");
+        ~TStack() {
+            delete[] data;
         }
-        return data[top--];
-    }
 
-    T watch() {
-        if (top < 0) {
-            throw std::underflow_error("Stack underflow");
+        void push(T value) {
+            if (top == size - 1) {
+                throw std::overflow_error("Stack overflow");
+            }
+            data[++top] = value;
         }
-        return data[top];
-    }
 
-    bool isEmpty() {
-        return top == -1;
-    }
+        T pop() {
+            if (top == -1) {
+                throw std::underflow_error("Stack underflow");
+            }
+            return data[top--];
+        }
 
-    bool isFull() {
-        return top == size - 1;
-    }
+        T watch() {
+            if (top < 0) {
+                throw std::underflow_error("Stack underflow");
+            }
+            return data[top];
+        }
+
+        bool isEmpty() {
+            return top == -1;
+        }
 };
 
 #endif  // INCLUDE_TSTACK_H_
